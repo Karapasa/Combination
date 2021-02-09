374d27d2915f176e5a9f5143fd61846f62d8a11f
Alex <alexmixpetrov@yandex.ru>
2021-02-09 19:19:06 +0300

    add req and rm forms

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class Parametr(FlaskForm):
-    name = StringField('Название параметра', validators=[DataRequired()])
-    exceptions = StringField('Исключения', validators=[DataRequired()])
-    submit = SubmitField('Сгенерировать данные')
