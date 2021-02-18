f7f81f0a4ca76c452f06cb46e20aecd6e3ca9049
Karapasa <alexmixpetrov@gmail.com>
2021-02-18 19:01:49 +0300

    logging

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
+import logging.config
+from settings import logging_config
 from app import app
 from app.utils.cobmination import res, moving_to_dir
 from flask import render_template, request,  make_response, send_file
+
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger('combination_logger')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -11,6 +16,7 @@ def generate():
     data = request.get_json(force=True)
     name = res(data['parlist'], data['exclist'])
     moving_to_dir(name)
+    logger.debug("Данные и исключения: %s | %s", data['parlist'], data['exclist'])
     return make_response(name)
 
 @app.route('/app/static/combs/<path>', methods=['GET'])
