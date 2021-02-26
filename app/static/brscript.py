eb8df725e90847fcfb127fb155e621c82980c4ff
Karapasa <alexmixpetrov@gmail.com>
2021-02-26 15:55:48 +0300

    change url

--- app/static/brscript.py
+++ app/static/brscript.py
@@ -156,7 +156,7 @@ class Comb:
 
 @bind(document['generate'], 'click')
 def generate(e):
-    url = 'http://127.0.0.1:5000/generate'
+    url = 'http://comb.karapasa.ru/generate'
     data = {'parlist': json.loads(storage['parlist']), 'exclist': json.loads(storage['exclist'])}
     ajax.post(url, data=json.dumps(data), oncomplete=output_data)
 
